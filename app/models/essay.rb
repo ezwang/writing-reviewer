b4667686b243d8e533f11d6a8043969f9fedc7b4
Eric Wang <ezwang1@gmail.com>
2018-04-01 14:05:11 -0400

    more essay validations

--- app/models/essay.rb
+++ app/models/essay.rb
@@ -2,4 +2,8 @@ class Essay < ApplicationRecord
   belongs_to :assignment, class: 'UserAssignment'
 
   has_many :reviewers, dependent: :destroy
+
+  validates :assignment, presence: true
+  validates :file, presence: true
+  validates_attachment :file, content_type: { content_type: ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'text/plain'] }
 end
