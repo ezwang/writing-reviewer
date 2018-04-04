add8eb0ab1c528871c1308504e110d355b07fde5
Eric Wang <ezwang1@gmail.com>
2018-04-04 13:15:44 -0400

    fix assignment display

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :given_assignments, class_name: 'Assignment', foreign_key: 'creator'
+  has_many :given_assignments, class_name: 'Assignment', foreign_key: 'creator_id'
   has_many :user_assignments, dependent: :destroy
   has_many :assignments, through: :user_assignment
   has_many :reviewed_essays, class_name: 'Review', dependent: :destroy
