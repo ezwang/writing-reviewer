2ccadf1eba4659eba6118df86bd7bf33a05aaf94
Eric Wang <ezwang1@gmail.com>
2018-04-11 14:56:27 -0400

    fix essay one validation

--- app/models/essay.rb
+++ app/models/essay.rb
@@ -4,7 +4,7 @@ class Essay < ApplicationRecord
   has_many :reviewers, dependent: :destroy
   has_attached_file :file
 
-  validates :user_assignment, presence: true
+  validates :user_assignment, presence: true, uniqueness: true
   validates :file, presence: true
   validates :file, attachment_content_type: { content_type: ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'text/plain'] }
 end
