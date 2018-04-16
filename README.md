9c4dfc4ed7aa6811c87aeb7eda622b51e65a3905
Eric Wang <ezwang1@gmail.com>
2018-04-16 18:47:41 -0400

    add mailgun config for production

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://www.travis-ci.org/ezwang/writing-reviewer.svg?branch=master)](https://www.travis-ci.org/ezwang/writing-reviewer)
 
-An essay reviewer application similar to the ["Penn Reviewer"](https://apps.sas.upenn.edu/sso/writing/cw/ng/dashboard) application used to peer review essays in writing seminar classes.
+An essay reviewer application similar to the [Penn Reviewer](https://apps.sas.upenn.edu/sso/writing/cw/ng/dashboard) application used to peer review essays in writing seminar classes.
 
 ## Setup
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Used to send emails.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'writing-reviewer.herokuapp.com',
+    authentication: :plain
+  }
 end
