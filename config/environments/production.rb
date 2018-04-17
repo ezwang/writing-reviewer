44a284c59ec930c3ea3a5049e4b1935c593a498e
Eric Wang <ezwang1@gmail.com>
2018-04-16 22:58:21 -0400

    add host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
 
   # Used to send emails.
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'writing-reviewer.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
     address: ENV['MAILGUN_SMTP_SERVER'],
