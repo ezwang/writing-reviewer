6ef9af28ceea85b633f769d161dae0f31a675ba5
Eric Wang <ezwang1@gmail.com>
2018-04-17 08:31:29 -0400

    more consistent naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }, controllers: { registrations: 'registrations' }
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }, controllers: { registrations: 'registrations' }
   get '/users', to: 'users#index'
 
   resources :assignments do
